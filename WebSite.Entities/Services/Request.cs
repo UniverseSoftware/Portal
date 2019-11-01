d3215f31b176c7eff3099ea4ad58f99c426a9169
Anderson Pedrassi <anderson_pedrassi@hotmail.com>
2019-11-01 18:47:31 +0000

    .

--- WebSite.Entities/Services/Request.cs
+++ WebSite.Entities/Services/Request.cs
@@ -13,13 +13,10 @@ namespace WebSite.Entities.Services
             HttpClient httpClient = new HttpClient();
             await httpClient.DeleteAsync(uri);
         }
-
         public async Task<TResult> GetAsync<TResult>(string uri)
         {
             try
             {
-
-
                 HttpClient httpClient = new HttpClient();
                 HttpResponseMessage response = await httpClient.GetAsync(uri);
 
@@ -33,12 +30,9 @@ namespace WebSite.Entities.Services
             }
             catch (Exception ex)
             {
-
                 throw;
             }
-
         }
-
         public async Task<TResult> PostAsync<TResult>(string uri, TResult data)
         {
             HttpClient httpClient = new HttpClient();
@@ -56,24 +50,6 @@ namespace WebSite.Entities.Services
 
             return result;
         }
-
-        //public async Task<TResult> PostAsync<TResult>(string uri, string data, string clientId)
-        //{
-        //    HttpClient httpClient = new HttpClient();
-
-        //                var content = new StringContent(data);
-        //    content.Headers.ContentType = new MediaTypeHeaderValue("application/x-www-form-urlencoded");
-        //    HttpResponseMessage response = await httpClient.PostAsync(uri, content);
-
-
-        //    string serialized = await response.Content.ReadAsStringAsync();
-
-        //    TResult result = await Task.Run(() =>
-        //        JsonConvert.DeserializeObject<TResult>(serialized));
-
-        //    return result;
-        //}
-
         public async Task<TResult> PutAsync<TResult>(string uri, TResult data)
         {
             HttpClient httpClient = new HttpClient();
